ebf5b6d3e4f784eb193c0a81974dfa72e3c5a0fa
atroopa <atroopa@gmail.com>
2023-05-06 11:15:08 +0330

    chang js to mj and import

--- example/ex2/app.js
+++
@@ -1,7 +0,0 @@
-import myModule from "./module";
-
-const print = console.log;
-
-print(next());
-print(next());
-print(prev());
---
+++ example/ex2/app.mjs
@@ -0,0 +1,7 @@
+import * as myModule from "./module.mjs";
+
+const print = console.log;
+
+print(myModule.next());
+print(myModule.next());
+print(myModule.prev());
