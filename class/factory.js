47b5be72bc876f1d462c9fd49102fcd682c328cb
atroopa <atroopa@gmail.com>
2023-05-06 18:43:13 +0330

    factory finish

--- class/factory.js
+++ class/factory.js
@@ -1,6 +1,17 @@
+const fish = {
+    color: "red",
+    maxSpeed: 20,
+    currentSpeed: 0,
 
-const fish1 = {
-    color:        "red",
-    maxSpeed:     20,
-    currentSpeed: 0
-}
+    up: function() {
+      if (this.currentSpeed < this.maxSpeed) {
+        this.currentSpeed++;
+      }
+    },
+    down: function() {
+      if (this.currentSpeed > 0) {
+        this.currentSpeed--;
+      }
+    }
+  };    
+  
