e5d5e992fc918bb4707bd480d3c6005996381c74
atroopa <atroopa@gmail.com>
2023-05-08 12:58:49 +0330

    event 1 finish

--- event/event1.js
+++ event/event1.js
@@ -5,6 +5,8 @@ import EventEmitter from 'events';
 
 const emitter = new EventEmitter();
 
+
+// Event 1 ,2 =========================================
 emitter.on("event1" , (...args) => {
     print(args);
 });
@@ -20,9 +22,16 @@ emitter.on("event2", (arg1, arg2) => {
 emitter.emit("event1", 1,2,3,4);
 emitter.emit("event2", 'sag', 'kir khar');
 
-
+//Event 3 =======================================
 emitter.once("event3", () => {
     print("EVENT3");
 });
 
 emitter.emit("event3");
+ 
+// Event Error ==========================================
+emitter.on("error", () => {
+    print("returned error here .. !");
+});
+
+emitter.emit("error");
