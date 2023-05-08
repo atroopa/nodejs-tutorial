b9d97143ff2772372e10484547f84af11fe4c7e6
atroopa <atroopa@gmail.com>
2023-05-08 12:47:44 +0330

    this is my first vist about event in js

--- event/event1.js
+++ event/event1.js
@@ -1,3 +1,12 @@
 console.clear();
 
+const print = console.log;
 import EventEmitter from 'events';
+
+const emitter = new EventEmitter();
+
+emitter.on("event1" , (...args) => {
+    print(args);
+});
+
+emitter.emit("event1", 1,2,3,4);
