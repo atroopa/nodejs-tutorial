705cb23bf4acbcde078615471f414e0cd483f7c5
atroopa <atroopa@gmail.com>
2023-05-08 10:51:43 +0330

    seter and geter work half

--- class/colorSeter.js
+++ class/colorSeter.js
@@ -11,4 +11,21 @@ class Color{
         this.b = b;
     }
 
+    setRed(value){
+        if(value >=0 && value <= 255){
+            this.r = parseInt(value);
+        } else {
+            throw new RangeError("value must be between 0 and 255");
+        }
+    }
+
+    getRed(){
+        return this.r;
+    }
+
 }
+
+const c1 = new Color();
+c1.setRed(100);
+print(c1);
+print(c1.getRed());
