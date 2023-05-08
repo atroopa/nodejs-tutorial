a708a78626b673fba0d072c7af894065453a321f
atroopa <atroopa@gmail.com>
2023-05-08 12:52:31 +0330

    on and emit object toturial

--- event/event1.js
+++ event/event1.js
@@ -9,4 +9,13 @@ emitter.on("event1" , (...args) => {
     print(args);
 });
 
+emitter.on("event1", args => {
+    print("Done...");
+});
+
+emitter.on("event2", (arg1, arg2) => {
+    print(arg1, arg2);
+});
+
 emitter.emit("event1", 1,2,3,4);
+emitter.emit("event2", 'sag', 'kir khar');
