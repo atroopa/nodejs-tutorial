a8f44edea263325a52d20b0a982217f56e28cba3
atroopa <atroopa@gmail.com>
2023-05-08 11:29:30 +0330

    add for hex into class

--- class/colorSeter.js
+++ class/colorSeter.js
@@ -1,22 +1,23 @@
 console.clear();
 
+import util from "util";
 
 const print = console.log;
 
-class Color{
+export default class Color{
 
     #r = 0;
     #g = 0;
     #b = 0;
 
     constructor(r=0 , g=0, b=0){
-        this.#r = r;
-        this.#g = g;
-        this.#b = b;
+        this.r = r;
+        this.g = g;
+        this.b = b;
         Object.seal(this);
     }
 
-    set red(value){
+    set r(value){
         if(value >=0 && value <= 255){
             this.#r = parseInt(value);
         } else {
@@ -24,8 +25,27 @@ class Color{
         }
     }
 
-    get red(){
+    get r(){
         return this.#r;
+    }
+
+
+    static forHex(hex){
+        const regex = new RegExp("^[0-9a-f]{6}$", "i");
+        if(!regex.test(hex)){
+            throw new Error("Invalid Hex Code : "+ hex);
+        }
+
+        return new Color (
+            parseInt(hex.substring(0,2), 16),
+            parseInt(hex.substring(2,4), 16),
+            parseInt(hex.substring(4,6), 16)
+
+        );
+    }
+    
+    [util.inspect.custom]() {
+        return `rgb(${this.r}, ${this.g} , ${this.b})`;
     }
 
 }
