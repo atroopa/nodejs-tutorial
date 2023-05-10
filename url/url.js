d65485027dd05ea0336de40afe9700ca76ba87fd
atroopa <atroopa@gmail.com>
2023-05-10 12:18:09 +0330

    learning url module

--- url/url.js
+++ url/url.js
@@ -7,3 +7,13 @@ import {URL} from "url";
 const myURL = new URL('https://user:pass@sub.example.com:8080/default.htm?year=2017&month=february#hash');
 
 print(myURL.href, "\n");
+print(myURL.hash);
+print(myURL.host);
+print(myURL.hostname);
+print(myURL.port);
+print(myURL.origin);
+print(myURL.username);
+print(myURL.password);
+print(myURL.pathname);
+print(myURL.protocol);
+print(myURL.search);
