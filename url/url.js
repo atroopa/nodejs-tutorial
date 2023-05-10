e9338e4aed79b38356ce0570a69924235afd684c
atroopa <atroopa@gmail.com>
2023-05-10 12:11:02 +0330

    a hard url fo example

---
+++ url/url.js
@@ -0,0 +1,7 @@
+console.clear()
+
+const print = console.log;
+
+import {URL} from "url";
+
+const myURL = new URL('https://user:pass@sub.example.com:8080/default.htm?year=2017&month=february#hash');
