8130efcbb00c53defa67fbe04e2c9c6472f710c1
atroopa <atroopa@gmail.com>
2023-05-10 17:10:19 +0330

    i create a yarn start by nodemon

--- packages/dotenv.js
+++ packages/dotenv.js
@@ -2,6 +2,7 @@ console.clear();
 
 const print = console.log;
 
+import chalk from "chalk";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -10,3 +11,5 @@ print(process.env.NUMBER);
 print(process.env.DB_USER);
 print(process.env.DB_PASS);
 
+print(chalk.yellow.inverse(process.env.COURSE));
+print(chalk.red.inverse(process.env.FULLNAME));
