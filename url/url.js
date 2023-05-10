9abaf03de63c469fbe49bde043d32ad7a62e6d2b
atroopa <atroopa@gmail.com>
2023-05-10 12:13:14 +0330

    show href of url

--- url/url.js
+++ url/url.js
@@ -5,3 +5,5 @@ const print = console.log;
 import {URL} from "url";
 
 const myURL = new URL('https://user:pass@sub.example.com:8080/default.htm?year=2017&month=february#hash');
+
+print(myURL.href, "\n");
