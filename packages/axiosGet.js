f15d00c29c2b74c4403d2103bd99401e91633503
atroopa <atroopa@gmail.com>
2023-05-11 11:29:07 +0330

    convert old function to arrow function

--- packages/axiosGet.js
+++ packages/axiosGet.js
@@ -3,14 +3,16 @@ console.clear();
 const print = console.log;
 
 import axios from "axios";
+import chalk from "chalk";
 
-async function getData() {
+const getData = async () => {
     try {
       const response = await axios.get('https://www.livemint.com/');
         
         //print(response.data);
         print("Status Code : " + response.status);
         print("Status Text : " + response.statusText);
+        print(chalk.red("Response Headers: " + response.headers));
 
     } catch (error) {
         print("error");
