135a92dbc5934f8c9877a4f03148c5116fd5f87c
atroopa <atroopa@gmail.com>
2023-05-11 12:01:19 +0330

    axios Get Finish

--- packages/axiosGet.js
+++ packages/axiosGet.js
@@ -5,14 +5,20 @@ const print = console.log;
 import axios from "axios";
 import chalk from "chalk";
 
+const config = {
+    url: 'https://jsonplaceholder.typicode.com/todos?_limit=4',
+    method: 'GET'
+}
+
+
 const getData = async () => {
     try {
-      const response = await axios.get('https://www.livemint.com/');
+      const response = await axios(config);
         
-        //print(response.data);
-        print("Status Code : " + response.status);
-        print("Status Text : " + response.statusText);
-        print(chalk.red("Response Headers: " + response.headers));
+        print(response.data);
+        //print("Status Code : " + response.status);
+        //print("Status Text : " + response.statusText);
+        //print(chalk.red("Response Headers: " + response.headers));
 
     } catch (error) {
         print("error");
